6db5265a7705e96b89af2a053b1aa3a10df080de
shaal <ofer@3paces.com>
2021-12-07 21:53:08 +0000

    test3

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -x
+
+npx -y cowsay "Preview browser should open by itself, without clicking inside VScode's Terminal panel"
+npx -y serve &
+gp await-port 3000
+sleep 1
+gp preview $(gp url 3000)
